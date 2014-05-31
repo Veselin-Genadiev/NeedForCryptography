5d543928e77fdc60ac9c6c5388181378429c28c3
Veselin-Genadiev <genadiev.veselin@gmail.com>
2014-05-31 18:11:43 +0300

    Working Playfair implementation

--- NeedForCryptography/Symmetric/Playfair.py
+++ NeedForCryptography/Symmetric/Playfair.py
@@ -1,14 +1,16 @@
 from math import ceil
 from Tools.double_dict import DoubleDict
 from re import findall
+from collections import OrderedDict
+from itertools import count
 
 
 class Playfair:
     def __init__(self, key):
-        self.__key = self.matrix_map(''.join(list(set(key))))
+        self.__key = self.matrix_map(''.join(list(OrderedDict.fromkeys(key))))
 
     def matrix_map(self, key):
-        rows = ceil(len(key) ** 0.5)
+        rows = int(ceil(len(key) ** 0.5))
         letter_map = [(letter, (index / rows, index % rows)) for
                       index, letter in enumerate(key)]
         return DoubleDict(letter_map)
@@ -18,19 +20,19 @@ class Playfair:
                         if self.__key.has_key(letter)])
 
         if len(text) % 2 == 1:
-            raise AttributeError("Text's symbols included in the key must be of even count!")
+            raise AttributeError("Text must be of even length!")
 
-        return ' '.join(self.__encrypt_pair(pair, offset) for pair
-                        in findall(r'..', text))
+        return ' '.join(self.__encrypt_pair(pair, offset)
+                        for pair in findall(r'..', text))
 
     def decrypt(self, text, offset=-1):
-        return self.encrypt(self, text, offset)
+        return self.encrypt(text, offset)
 
     def __encrypt_pair(self, pair, offset=1):
         first, second = pair
 
         if(first == second):
-            return (first, second)
+            return pair
 
         first_row, first_col = self.__key.get_value(first)
         second_row, second_col = self.__key.get_value(second)
@@ -46,36 +48,48 @@ class Playfair:
         return self.__encrypt_pair(pair, -1)
 
     def __cyclic_shift_row(self, row, col, offset):
-        if not self.__key.has_value((row + offset, col)):
+        if self.__key.has_value((row + offset, col)):
             return (row + offset, col)
+        elif row == 0 and offset < 0:
+            for index in count():
+                if not self.__key.has_value((index, col)):
+                    return (index - 1, col)
         else:
             return (0, col)
 
     def __cyclic_shif_col(self, row, col, offset):
-        if not self.__key.has_value((row, col + offset)):
+        if self.__key.has_value((row, col + offset)):
             return (row, col + offset)
+        elif col == 0 and offset < 0:
+            for index in count():
+                if not self.__key.has_value((row, index)):
+                    return (row, index - 1)
         else:
             return (row, 0)
 
     def __cyclic_vertical(self, pair, offset):
         first, second = pair
-        first_row, first_col = \
-            self.__cyclic_shift(self.__key.get_value(first), offset)
-        second_row, second_col = \
-            self.__cyclic_shift(self.__key.get_value(second), offset)
+        first_row, first_col = self.__key.get_value(first)
+        first_coordinates = \
+            self.__cyclic_shift_row(first_row, first_col, offset)
+        second_row, second_col = self.__key.get_value(second)
+        second_coordinates = \
+            self.__cyclic_shift_row(second_row, second_col, offset)
 
-        return (self.__key.get_key((first_row, first_col)),
-                self.__key.get_key((second_row, second_col)))
+        return self.__key.get_key(first_coordinates) + \
+            self.__key.get_key(second_coordinates)
 
     def __cyclic_horizontal(self, pair, offset):
         first, second = pair
-        first_row, first_col = \
-            self.__cyclic_shif_col(self.__key.get_value(first), offset)
-        second_row, second_col = \
-            self.__cyclic_shif_col(self.__key.get_value(second), offset)
+        first_row, first_col = self.__key.get_value(first)
+        first_coordinates = \
+            self.__cyclic_shif_col(first_row, first_col, offset)
+        second_row, second_col = self.__key.get_value(second)
+        second_coordinates = \
+            self.__cyclic_shif_col(second_row, second_col, offset)
 
-        return (self.__key.get_key((first_row, first_col)),
-                self.__key.get_key((second_row, second_row)))
+        return self.__key.get_key(first_coordinates) + \
+            self.__key.get_key(second_coordinates)
 
     def __switched_columns(self, pair):
         first, second = pair
@@ -83,5 +97,5 @@ class Playfair:
         second_row, second_col = self.__key.get_value(second)
         first_col, second_col = second_col, first_col
 
-        return (self.__key.get_key((first_row, first_col)),
-                self.__key.get_key((second_row, second_col)))
+        return self.__key.get_key((first_row, first_col)) + \
+            self.__key.get_key((second_row, second_col))
