ad40d5dca92836a3f907e22607a6241da60fe237
Veselin-Genadiev <genadiev.veselin@gmail.com>
2014-06-02 02:48:14 +0300

    Implement L.Hill cipher (not tested). Using numpy for linear algebra operations.

--- NeedForCryptography/Symmetric/hill.py
+++ NeedForCryptography/Symmetric/hill.py
@@ -1,19 +1,42 @@
-import Tools.utilities as utilities
+from string import ascii_uppercase
+from collections import OrderedDict
+from numpy.linalg import inv, det, dot
 
 
 class Hill:
     '''
     Hill cipher. For encrypt/decrypt we need an invertible square matrix of
-    indexes with length of [0, 25] and a rank eqaul to the length of the text
+    elements with length of [0, 25] and a rank eqaul to the length of the text
     which will be encrypted. The algorithm is ment to be used for encrypting
-    a single word only.
+    a single word only. By default English uppercase alphabet is used.
     '''
-    def __init__(self, matrix):
-        if utilities.det(matrix) == 0:
+    def __init__(self, matrix, alphabet=ascii_uppercase):
+        if det(matrix) == 0:
             raise ValueError('Determinant must be invertible')
 
-        self.__matrix = matrix
+        self.__alphabet = ''.join(list(OrderedDict.fromkeys(alphabet)))
+        self.__modulo = len(self.__alphabet)
+        self.__matrix = [map(lambda col: col % self.__modulo, row)
+                         for row in matrix]
 
-    def encrypt(text):
-        text = text.upper()
-        return text
+    def encrypt(self, text, encryption=True):
+        vector = self.__vectorize(text)
+        matrix = self.__matrix
+
+        if not encryption:
+            matrix = inv(self.__matrix)
+
+        encrypted_vector = dot(matrix, vector)
+        encrypted_vector = [map(lambda col: col % self.__modulo, row)
+                            for row in encrypted_vector]
+        encrypted_text = self.__textify(encrypted_vector)
+        return encrypted_text
+
+    def decrypt(self, text):
+        return self.encrypt(text, False)
+
+    def __vectorize(self, text):
+        return [self.__alphabet.index(symbol) for symbol in text]
+
+    def __textify(self, vector):
+        return ''.join(self.__alphabet[i] for i in vector)
