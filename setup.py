01a7fe9d95dad5a8301c8a2f0850976fa6d3f363
Veselin-Genadiev <genadiev.veselin@gmail.com>
2014-06-27 03:12:42 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+
+setup_info = {'name': 'NeedForCryptography',
+              'description': 'Python cryptographic tools and ciphers',
+              'author': 'Veselin Genadiev',
+              'author_email': 'genadiev.veselin@gmail.com',
+              'url': 'https://www.facebook.com/genadiev.veselin',
+              'packages': ['lib', 'lib.symmetric, lib.asymmetric',
+                           'lib.tools', 'lib.tests'],
+              'package_dir': {'lib': 'lib'},
+              'classifiers': [
+                  'Development Status :: 4 - Beta',
+                  'Environment :: Console',
+                  'Environment :: Web Environment',
+                  'Intended Audience :: End Users/Desktop',
+                  'Intended Audience :: Developers',
+                  'Intended Audience :: System Administrators',
+                  'License :: OSI Approved ::'
+                  ' Python Software Foundation License',
+                  'Operating System :: MacOS :: MacOS X',
+                  'Operating System :: Microsoft :: Windows',
+                  'Operating System :: POSIX',
+                  'Programming Language :: Python',
+                  'Topic :: Communications :: Email',
+                  'Topic :: Office/Business',
+                  'Topic :: Software Development :: Bug Tracking',
+              ],
+              }
+
+
+setup(setup_info)
